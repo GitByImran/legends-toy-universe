89d8aba2eb42f6f92dd7aa4fd4902bfb0f9f2873
imran-h-ob <imran.h.ovi.61@gmail.com>
2023-05-21 23:20:52 +0600

    sort by price system applied

--- src/components/my-toys/MyToys.jsx
+++ src/components/my-toys/MyToys.jsx
@@ -7,17 +7,21 @@ const MyToys = () => {
   const { user } = useContext(AuthContext);
   const [details, setDetails] = useState([]);
   const [updateTrigger, setUpdateTrigger] = useState(false);
+  const [sorting, setSorting] = useState({ sortBy: "price", order: "asc" });
 
   useEffect(() => {
-    fetch(
-      `https://legends-toy-universe-server.vercel.app/toys?email=${user?.email}`
-    )
+    fetchToys();
+  }, [user, sorting, updateTrigger]);
+
+  const fetchToys = () => {
+    const url = `https://legends-toy-universe-server.vercel.app/toys?email=${user?.email}&sort=${sorting.sortBy}&order=${sorting.order}`;
+    fetch(url)
       .then((res) => res.json())
       .then((data) => setDetails(data));
-  }, [user]);
+  };
 
   const handleDelete = (id) => {
-    const proceed = confirm("delete ???????");
+    const proceed = window.confirm("Are you sure you want to delete this toy?");
     if (proceed) {
       fetch(`https://legends-toy-universe-server.vercel.app/toys/${id}`, {
         method: "DELETE",
@@ -36,26 +40,34 @@ const MyToys = () => {
     setUpdateTrigger((prevTrigger) => !prevTrigger);
   };
 
-  useEffect(() => {
-    fetch(
-      `https://legends-toy-universe-server.vercel.app/toys?email=${user?.email}`
-    )
-      .then((res) => res.json())
-      .then((data) => setDetails(data));
-  }, [user, updateTrigger]);
+  const handleSorting = (sortBy) => {
+    const newOrder =
+      sorting.sortBy === sortBy && sorting.order === "asc" ? "desc" : "asc";
+    setSorting({ sortBy, order: newOrder });
+  };
 
   return (
     <div>
-      <p className="text-center">Displaying your toys : {details.length}</p>
+      <p className="text-center">Displaying your toys: {details.length}</p>
       <Container>
+        <div className="sorting-options">
+          <button
+            onClick={() => handleSorting("price")}
+            className={sorting.sortBy === "price" ? "active" : ""}
+          >
+            Sort by Price{" "}
+            {sorting.order === "asc" ? "▲ low to high" : "▼ hight to low"}
+          </button>
+          {/* Add more sorting options if needed */}
+        </div>
         {details.map((toys) => (
           <MyToysCard
             key={toys._id}
             toys={toys}
             handleDelete={handleDelete}
             setUpdateTrigger={setUpdateTrigger}
-            handleUpdateSuccess={handleUpdateSuccess} // Pass the setUpdateTrigger function as a prop
-          ></MyToysCard>
+            handleUpdateSuccess={handleUpdateSuccess}
+          />
         ))}
       </Container>
     </div>
